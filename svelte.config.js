f3ddf4bed9a481f4229c9025117aafcc46437b86
geardy47 <muhammadgenta14@gmail.com>
2025-03-31 22:43:37 +0700

    chore: setup config for mdsvex

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,19 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+import { mdsvex } from 'mdsvex';
+
+/** @type {import('mdsvex').MdsvesOptions} */
+const mdsvexOptions = {
+	extensions: ['.md']
+};
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	extensions: ['.svelte', '.md'],
+	preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
 
 	kit: {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
