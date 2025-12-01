8c654695cfc8bf8447328c9748679a7d2306f500
geardy47 <muhammadgenta14@gmail.com>
2025-12-01 15:45:59 +0700

    feat: implement sidebar and default dark mode

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,59 +1,32 @@
 <script lang="ts">
-	import Sidebar from './sidebar.svelte';
+	import { ModeWatcher, setMode } from 'mode-watcher';
+	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+
+	import './layout.css';
+	import AppSidebar from './app-sidebar.svelte';
 	import Footer from './footer.svelte';
-
-	import 'open-props/style';
-	import 'open-props/normalize';
-	import 'open-props/buttons';
-	import '../app.css';
-
 	// import * as config from '$lib/config';
 
+	setMode('dark');
 	let { children } = $props();
-	// let { children, data } = $props()
 </script>
 
-<div class="data">
-	<div class="layout">
-		<aside>
-			<Sidebar />
-		</aside>
-
-		<main>
-			<div class="content">
-				{@render children?.()}
-			</div>
-			<div class="footer">
-				<Footer />
-			</div>
-		</main>
-	</div>
-</div>
+<ModeWatcher />
+<Sidebar.Provider>
+	<AppSidebar />
+	<main>
+		<div class="content">
+			<Sidebar.Trigger />
+			{@render children?.()}
+		</div>
+		<div class="footer">
+			<Footer />
+		</div>
+	</main>
+	<!-- <main> -->
+	<!--   {@render children?.()} -->
+	<!-- </main> -->
+</Sidebar.Provider>
 
 <style>
-	.data {
-		width: 100%;
-		margin-left: auto;
-		margin-right: auto;
-	}
-
-	.layout {
-		display: flex;
-		min-height: 100%;
-		width: 100%;
-		box-sizing: border-box;
-
-		main {
-			padding-block: var(--size-fluid-6);
-			margin-right: auto;
-		}
-
-		aside {
-			margin-left: auto;
-			position: sticky;
-			top: 0;
-			align-self: flex-start;
-			padding-right: var(--size-fluid-6);
-		}
-	}
 </style>
